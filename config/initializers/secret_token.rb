c153a9ccbe3c304287382df350da5b94f7768a3a
NebsNebbier <NebsNebbier@gmail.com>
2013-04-26 09:55:24 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jibes::Application.config.secret_token = 'e40c7a53c35324844e3fb9dc9a85b6928a9f20f73ebde0f4668f31a14552bb7f132a04cb7fe6134d13b3230447c8bbae8053a8151c09bbf1945b909fdb739c4a'
